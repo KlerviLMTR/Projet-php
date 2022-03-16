f34cce42e95c71a5e8e893858ec49d9ac8e3caf1
ymarkuc <62643532+ymarkuc@users.noreply.github.com>
2022-03-16 16:59:06 +0100

    modification match + ajout equipe adverse

--- html-php/match/affichage_matchs.php
+++ html-php/match/affichage_matchs.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
     foreach($resultat as $match){
 
         $id_m = $match['Id_match_'];
@@ -11,6 +13,11 @@
         <div class="date">
             <p>Date du match : '.$match["date_match"].' '.$match["heure_match"].'</p>
         </div>
+
+        <div class="equipe_adverse">
+            <p>Equipe adverse : '.$match["equipe_adverse"].'</p>
+        </div>
+
 
         <div class="lieu">
             <p>'.$match["lieu"].'</p>
@@ -28,9 +35,9 @@
 
        echo '
         <div class="footer">
-            <a href="./match_formulaire.php?v1='.$match["Id_match_"].'&v2='.$match["date_match"].'&v3='.$match["heure_match"].'&v4='.$match["lieu"].'&v5='.$match["score_equipe"].'&v6='.$match["score_adverse"].'
+            <a href="./match_formulaire.php?v1='.$match["Id_match_"].'&v2='.$match["date_match"].'&v3='.$match["heure_match"].'&v4='.$match["lieu"].'&v5='.$match["score_equipe"].'&v6='.$match["score_adverse"].'&v7='.$match["equipe_adverse"].'
             ">✎</a>
-            <a href="./matchs.php?idsupp='.$match["Id_match_"].'">🗑</a>
+            <a href="./joueurs.php?idsupp='.$match["Id_match_"].'">🗑</a>
         </div>
 
         ';
--- html-php/match/match_formulaire.php
+++ html-php/match/match_formulaire.php
@@ -15,25 +15,51 @@
     <?php
         include_once("../config.php");
         include_once("./match_formulaire_requetes.php");
+        include_once("./modifier_match.php");
     ?>
+    
     <form action="./match_formulaire.php" method="POST">
         <h1>Ajouter un nouveau match :</h1>
         <label for="date">Date du match :</label>
-        <input type="date" name="date">
+        <input type="date" name="date"
+            <?php
+                pr_date_match();
+            ?>
+        >
         <br>
         <label for="heure">Heure du match :</label>
-        <input type="time" name="heure">
+        <input type="time" name="heure"
+            <?php
+                pr_heure_match();
+            ?>
+        >
         <br>
         <label for="equipe_adverse">Equipe adverse :</label>
-        <input type="text" name="equipe_adverse">
+        <input type="text" name="equipe_adverse"
+            <?php
+                pr_equipe_adverse();
+            ?>
+        >
         <br>
-        <input type="radio" name="lieu_rencontre" value="domicile">
+        <input type="radio" name="lieu_rencontre" value="domicile"
+            <?php
+                pr_lieu_match_dom();
+            ?>
+        >
         <label for="domicile">Domicile</label>
         <br>
-        <input type="radio" name="lieu_rencontre" value="exterieur">
+        <input type="radio" name="lieu_rencontre" value="exterieur"
+            <?php
+                pr_lieu_match_ext();
+            ?>
+        >
         <label for="exterieur">Exterieur</label>
         <br>
-        <input type="text" hidden name="Id_match">
+        <input type="text" hidden name="Id_match"
+            <?php
+                pr_id_match();
+            ?>
+        >
         <input type="submit" value="valider">
 
     </form>
