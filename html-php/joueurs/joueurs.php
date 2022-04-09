5e073a692b838c5fe644050b6c9ac913849281b9
Florianne CORREIA <florianne.correia@protonmail.com>
2022-04-09 11:41:12 +0200

    fonction recherche

--- html-php/joueurs/joueurs.php
+++ html-php/joueurs/joueurs.php
@@ -33,30 +33,72 @@
             <h1>Liste des joueurs :</h1>
             <hr id="main_sep">
             <a href="../ajout_modif/formulaire.php">Lien pour ajouter un joueur</a>
-            <?php
 
-                //Vérfier si un joueur a été supprimé:
-                supprimer_joueur($pdo);
-
-                //Récupérer toutes les données nécessaires à l'affichage des joueurs:
-                $sql = "select * from joueur ";
-                $prep = $pdo->prepare($sql);
-                $prep -> execute();
-                $resultat = $prep->fetchAll();
+            <form action="" method="POST">
+                Rechercher 
+                <input type="text" name="recherche" value="<?php if(!empty($_POST['recherche'])) echo $_POST['recherche'] ?>">
+                <input type="submit" value="Envoyer">
+            </form>
             
-                //Affichage des joueurs :
-                include_once('affichage_joueurs.php');
+
+
+            <?php
+                //Si une recherche a été effectuée
+                if(!empty($_POST['recherche'])) {
+                    $sql="SELECT * FROM joueur WHERE ";
+                    $symb = " ";
+                    $tab = explode($symb, $_POST['recherche']);
+                    $cpt = 1;
+
+                    foreach($tab as $mot) {
+                        if (!($cpt == sizeof($tab))) {
+                            $sql.='UPPER(nom) like UPPER(\'%'.$mot.'%\') OR UPPER(prenom) like UPPER(\'%'.$mot.'%\') OR UPPER(poste_prefere) like UPPER(\'%'.$mot.'%\') OR 
+                                UPPER(statut) like UPPER(\'%'.$mot.'%\') OR ';
+                        } else {
+                            $sql.='UPPER(nom) like UPPER(\'%'.$mot.'%\') OR UPPER(prenom) like UPPER(\'%'.$mot.'%\') OR UPPER(poste_prefere) like UPPER(\'%'.$mot.'%\') OR 
+                                UPPER(statut) like UPPER(\'%'.$mot.'%\')';
+                        }
+                        $cpt++;
+                    }
+                    $prep = $pdo->prepare($sql);
+                    $prep->execute();
+                    $resultat = $prep->fetchAll(PDO::FETCH_ASSOC);
+
+                    if (empty($resultat)) {
+                        echo "Aucun résultat.<br>
+                                Vous pouvez effectuer une recherche par nom, prénom, poste ou statut.<br>
+                                <button onclick=\"location.href='./joueurs.php'\">Retour</button>";
+                    } else {  
+                        echo "<button onclick=\"location.href='./joueurs.php'\">Retour</button>";                      
+                        //Affichage des joueurs trouvés :
+                        include_once('affichage_joueurs.php');
+                    }
+
+                } else {
+                //Affichage sans recherche
+                    //Vérfier si un joueur a été supprimé:
+                    supprimer_joueur($pdo);
+
+                    //Récupérer toutes les données nécessaires à l'affichage des joueurs:
+                    $sql = "select * from joueur ";
+                    $prep = $pdo->prepare($sql);
+                    $prep -> execute();
+                    $resultat = $prep->fetchAll();
+                
+                    //Affichage des joueurs :
+                    include_once('affichage_joueurs.php');
+                }
                 
             
             ?>
         
         </main>
     </div>
-    <footer>
+    <!-- <footer>
         <a href="../mentions_leg.html">Mentions légales</a>
         <a href="../sources.html">Sources des images</a>
         <a href="../contact.php">Nous contacter</a>
-    </footer>
+    </footer> -->
 
 
 
